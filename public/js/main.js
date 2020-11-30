2ff5f34763fc08472efe907716716ac438cb32b3
Raymond Baguma <you@example.com>
2020-11-30 14:40:30 +0100

    applying scrollspy

--- public/js/main.js
+++ public/js/main.js
@@ -7,9 +7,13 @@ const toggleMenu = document.getElementById('nav-toggle');
 const closeMenu = document.getElementById('nav-close');
 const closeButton = document.querySelector('#nav-close .fas');
 const spinnerIcon = document.getElementById('spinner');
+const cards = document.querySelectorAll('#cards_services');
+const links = document.querySelectorAll('.nav_list');
 
-const allNavLinks = document.querySelectorAll('.nav_list a');
 
+// card animation
+flipCard();
+// animation module
 animation_module;
 // Show
 toggleMenu.addEventListener('click', () => {
@@ -37,6 +41,65 @@ const navLink = document.querySelectorAll('.nav_link');
 function linkAction() {
     navMenu.classList.remove('show');
 }
+
+// flip cards funtion
+function flipCard() {
+    cards.forEach(card => {
+        $(card).hover(() => {
+            $(card).addClass('animated pulse');
+        }, () => {
+            $(card).removeClass('animated pulse');
+        });
+    })
+};
+// handle clicks on links
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-// ScrollSpy
+// ----------------------------------------------------------------
+// scrollSpy
+// Cache selectors
+var lastId,
+    topMenu = $(links),
+    topMenuHeight = topMenu.outerHeight() + 15,
+
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function() {
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e) {
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function() {
+    // container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // id of current scroll item
+    var cur = scrollItems.map(function() {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        //remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
+    }
+});
